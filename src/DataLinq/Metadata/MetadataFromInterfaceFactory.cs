fb2fcc45a6950bcdcdb431c238b70e83c344a338
Sebastian Öberg <sebastian.oberg@gmail.com>
2022-03-09 21:41:48 +0100

    Added all the simple types.

--- src/DataLinq/Metadata/MetadataFromInterfaceFactory.cs
+++ src/DataLinq/Metadata/MetadataFromInterfaceFactory.cs
@@ -226,10 +226,32 @@ namespace DataLinq.Metadata
         {
             switch (type.Name)
             {
+                case "SByte":
+                    return "sbyte";
+                case "Byte":
+                    return "byte";
+                case "Int16":
+                    return "short";
+                case "UInt16":
+                    return "ushort";
                 case "Int32":
                     return "int";
+                case "UInt32":
+                    return "uint";
                 case "Int64":
                     return "long";
+                case "UInt64":
+                    return "ulong";
+                case "Char":
+                    return "char";
+                case "Single":
+                    return "float";
+                case "Double":
+                    return "double";
+                case "Boolean":
+                    return "bool";
+                case "Decimal":
+                    return "decimal";
                 default:
                     return type.Name;
             }
@@ -242,11 +264,18 @@ namespace DataLinq.Metadata
 
             return csType switch
             {
-                "bool" => sizeof(bool),
+                "sbyte" => sizeof(sbyte),
+                "byte" => sizeof(byte),
+                "short" => sizeof(short),
+                "ushort" => sizeof(ushort),
                 "int" => sizeof(int),
+                "uint" => sizeof(uint),
                 "long" => sizeof(long),
+                "ulong" => sizeof(ulong),
+                "char" => sizeof(char),
                 "float" => sizeof(float),
                 "double" => sizeof(double),
+                "bool" => sizeof(bool),
                 "decimal" => sizeof(decimal),
                 "DateTime" => 8,
                 "DateOnly" => sizeof(int),
