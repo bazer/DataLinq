5b1bda6f3de10f318795b32eda61c76eaa14348d
Sebastian Öberg <sebastian.oberg@gmail.com>
2022-07-10 23:44:15 +0200

    Changed to correct database.

--- src/DataLinq.Tests/Models/employees/Views/dept_emp_latest_date.cs
+++ src/DataLinq.Tests/Models/employees/Views/dept_emp_latest_date.cs
@@ -6,7 +6,7 @@ using DataLinq.Attributes;
 
 namespace DataLinq.Tests.Models
 {
-    [Definition("select `employees`.`dept_emp`.`emp_no` AS `emp_no`,max(`employees`.`dept_emp`.`from_date`) AS `from_date`,max(`employees`.`dept_emp`.`to_date`) AS `to_date` from `employees`.`dept_emp` group by `employees`.`dept_emp`.`emp_no`")]
+    [Definition("select `datalinq_employees`.`dept_emp`.`emp_no` AS `emp_no`,max(`datalinq_employees`.`dept_emp`.`from_date`) AS `from_date`,max(`datalinq_employees`.`dept_emp`.`to_date`) AS `to_date` from `datalinq_employees`.`dept_emp` group by `datalinq_employees`.`dept_emp`.`emp_no`")]
     [Name("dept_emp_latest_date")]
     public partial record dept_emp_latest_date : IViewModel
     {
