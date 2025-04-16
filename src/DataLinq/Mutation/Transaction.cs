4bf4a943943f646d4df9576930c919b30f97794b
Sebastian Öberg <sebastian.oberg@gmail.com>
2025-04-17 01:50:54 +0200

    Removed a few package dependencies that were not needed anymore.

--- src/DataLinq/Mutation/Transaction.cs
+++ src/DataLinq/Mutation/Transaction.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading;
-using CommunityToolkit.HighPerformance;
 using DataLinq.Exceptions;
 using DataLinq.Instances;
 using DataLinq.Interfaces;
--- src/DataLinq/Query/Select.cs
+++ src/DataLinq/Query/Select.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using CommunityToolkit.HighPerformance;
 using DataLinq.Extensions.Helpers;
 using DataLinq.Instances;
 using DataLinq.Metadata;
